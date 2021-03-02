e3c9db5fa8556d0ec088e9dde5811e7090152423
Saad Kureshi <saq619@gmail.com>
2021-03-02 15:16:11 -0500

    seeds added

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Saad', 'Kureshi', 'saad@saad.com', '$2b$12$qhfZKizwHA6fjZjS16OUWO43WGNMuxaZ6ot0ire5uPPh60oDrNHQK')
+VALUES
+('Saad', 'Kureshi', 'saad@saad.com', '$2b$12$qhfZKizwHA6fjZjS16OUWO43WGNMuxaZ6ot0ire5uPPh60oDrNHQK')
+('Bob', 'Smith', 'bob@bob.com', '$2b$12$qhfZKizwHA6fjZjS16OUWO43WGNMuxaZ6ot0ire5uPPh60oDrNHQK')
